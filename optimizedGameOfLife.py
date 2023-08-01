af49bf78948b2bfe70df9976197be44f5590491f
Reinhard9x <140282388+Reinhard9x@users.noreply.github.com>
2023-08-01 19:37:52 +0200

    Update optimizedGameOfLife.py

--- optimizedGameOfLife.py
+++ optimizedGameOfLife.py
@@ -2,7 +2,79 @@ from tkinter import *
 import random
 import time
 
-num = 30
+
+def status_check():
+    #if the square is in the first column the 3 squares to the left will be automatically considered white
+    if j in first_column:
+        cont = 3
+    #these make sure it will consider only the same column and the one to the right
+        a = -1
+        b = 2
+        c = 0
+        d = 2
+        cont = Count(a, b, c, d, cont)
+        return cont
+    # if the square is in the last column the 3 squares to the right will be automatically considered white
+    elif j in last_column:
+        cont = 3
+        a = -1
+        b = 2
+        c = -1
+        d = 1
+        cont = Count(a, b, c, d, cont)
+        return cont
+    #same concept but for squares in the first or last row
+    elif i == 0:
+        cont = 3
+        a = 0
+        b = 2
+        c = -1
+        d = 2
+        cont = Count(a, b, c, d, cont)
+        return cont
+    elif i == num-1:
+        cont = 3
+        a = -1
+        b = 1
+        c = -1
+        d = 2
+        cont = Count(a, b, c, d, cont)
+        return cont
+    else:
+        cont = 0
+        a = -1
+        b = 2
+        c = -1
+        d = 2
+        cont = Count(a, b, c, d, cont)
+        return cont
+
+
+def Count(a, b, c, d, cont):
+    for n in range(a, b):
+        for m in range(c, d):
+            # exclude the square we are looking to change from consideration
+            if m == n == 0:
+                continue
+            # check the color of the squares around it
+            try:
+                if list[i + n][j + m] == 'white':
+                    cont += 1
+            except IndexError:#this takes care of the 4 squares in the top left/right and bottom left/right
+                cont += 1
+    return cont
+
+
+num = 20
+#check if square is on the first or last. this is needed to make sure that it doesn't take into consideration
+#squares on the opposite side of the grid in the rows above or below when checking if it needs to change color.
+#it becomes clear why its needed if you remove it
+first_column = []
+last_column = []
+for i in range(0, ((num*(num-1))+1), num):
+    first_column.append(i)
+for i in range(num-1, num*num, num):
+   last_column.append(i)
 
 window = Tk()
 window.title('Game Of Life')
@@ -32,33 +104,13 @@ while True:
     for i in range(num):
         for j in range(num):
             if list[i][j] == 'white':
-                cont = 0
-                for n in range(-1, 2):
-                    for m in range(-1, 2):
-                        # exclude the square we are looking to change from consideration
-                        if m == n == 0:
-                            continue
-                        # check the color of the squares around it
-                        try:
-                            if list[i + n][j + m] == 'white':
-                                cont += 1
-                        except IndexError:
-                            cont += 1
+                cont = status_check()
                 # saves the squares that need to go from white to black
                 if cont == 5:
                     listw2.append(i)
                     listw3.append(j)
             if list[i][j] == 'black':
-                cont = 0
-                for n in range(-1, 2):
-                    for m in range(-1, 2):
-                        if m == n == 0:
-                            continue
-                        try:
-                            if list[i + n][j + m] == 'white':
-                                cont += 1
-                        except IndexError:
-                            cont += 1
+                cont = status_check()
                 # squares that need to go from black to white
                 if cont != 5 and cont != 6:
                     listb2.append(i)
